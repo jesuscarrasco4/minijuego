afd58d248e304b2a9626a03394699169bd77016d
jesuscarrasco4 <jesusscarrassco4@gmail.com>
2024-08-21 16:50:49 +0200

    Add files via upload

---
+++ juego_python/item.py
@@ -0,0 +1,24 @@
+import pygame
+
+class Item:     # Esta va a ser la clase que dara forma al item del juego
+    def __init__(self, x, y):   # Esta funcion va a recibir un self (como todos los __init__), va a recebir tambien una "x" y una "y"
+        self.x = x              # Entonces vamos a decir que su posicion "x" va a ser igual a "x"
+        self.y = y
+        self.ancho = 40         # Aqui le pondremos el tamaño que tendra nuestro item
+        self.alto = 40
+        self.velocidad = 5      # La velocidad a la que se movera el item - Al cambiar los FPS, si es necesario, tenemos que cambiar la velocidad
+        self.color = "green"     # El colo que tendra el item
+        self.rect = pygame.Rect(self.x, self.y,             # "rect", es la forma que tiene pygame de crear objetos y 
+                                self.ancho, self.alto)      # definir sus dimensiones, coords ...
+        self.vida = 3
+        self.imagen = pygame.image.load("images/item.png")
+        self.imagen = pygame.transform.scale(self.imagen,(self.ancho, self.alto))
+
+    
+    def dibujar(self, ventana):     # Lo que va a hacer esta funcion es que va a dibujar nuestro rectangulo - (Ventana, se refiere al cod "pygame1.py")
+        self.rect = pygame.Rect(self.x, self.y,            # Esta linea tambien deberiamos llamarla en la duncion dibujar, para que 
+                                self.ancho, self.alto)     # se el personaje se mueva sin problemas
+        ventana.blit(self.imagen, (self.x, self.y))
+        
+    def movimiento(self):   #Lo que hara esta linea, sera mover el item de una sola manera, de arriba hacia abajo
+        self.y += self.velocidad
--- juego_python/pygame1.py
+++ juego_python/pygame1.py
@@ -1,42 +1,47 @@
 import pygame
 import random
 
-from personaje import Cubo  # Importamos el codigo de "personaje.py"
-from enemigo import Enemigo  # Importamos el codigo de "enemigo.py"
-from bala import Bala  # Importamos el codigo de "bala.py"
+from personaje import Cubo  # Importamos el código de "personaje.py"
+from enemigo import Enemigo  # Importamos el código de "enemigo.py"
+from item import Item  # Importamos el código de "item.py"
+from bala import Bala  # Importamos el código de "bala.py"
 
 pygame.init()  # Inicializar Pygame
+pygame.mixer.init()
 
 ANCHO = 1000
 ALTO = 800
-VENTANA = pygame.display.set_mode([ANCHO, ALTO])  # .display => Lo usamos para referirnos a todo lo que tiene que ver con la pantalla
-FPS = 60  # Para el uso del time.Clock(), tenemos que definir los fps, para que en todos los ordenadores vaya a los mismos FPS
-FUENTE = pygame.font.SysFont("Urban", 40)  # Aqui vamos a poner la fuente que queremos que tenga nuestro juego, y el tamaño en px (La tipografia)
+VENTANA = pygame.display.set_mode([ANCHO, ALTO])  # Configurar la ventana del juego
+FPS = 60  # Definir los frames por segundo
+FUENTE = pygame.font.SysFont("Urban", 40)  # Fuente y tamaño para textos en pantalla
+SONIDO_DISPARO = pygame.mixer.Sound("audio\disparo.wav")
+SONIDO_MUERTE = pygame.mixer.Sound("audio\muerte.wav")
 
 # Cargar y escalar la imagen de fondo
 fondo = pygame.image.load("images/fondo.png")  # Ruta de la imagen de fondo
 fondo = pygame.transform.scale(fondo, (ANCHO, ALTO))  # Escalar la imagen al tamaño de la ventana
 
-
-# Para que la pantalla no se cierre automaticamente, tendremos que utilizar un bucle:
-# [Bucle principal del juego]
-
 jugando = True
 
-reloj = pygame.time.Clock()  # Tenemos que crear mas enemigos, por ello, vamos a crear un reloj para que ada cierto tiempo se genere un enemigo
+reloj = pygame.time.Clock()
 
-vida = 5  # Vamos ha hacer un sistemas de vidas, en el que tendremos una cantidad de vidas
-puntos = 0  # Vamos ha hacer un sistemas de puntos
+vida = 5  # Vidas iniciales del jugador
+puntos = 0  # Puntuación inicial del jugador
 
-tiempo_pasado = 0  # Tenemos que ver el tiempo que ha pasado de un punto a otro para asi crear nuevos enemigos
-tiempo_entre_enemigos = 500  # Esto va a ser la cantidad de milisegundos que tienen que pasar desde que aparece un enemigos hasta que aparece otro
+tiempo_pasado_enemigos = 0  # Tiempo transcurrido desde la aparición del último enemigo
+tiempo_entre_enemigos = 500  # Tiempo entre la aparición de enemigos en milisegundos
 
-cubo = Cubo(ANCHO / 2, ALTO - 80)  # Hemos quitado que el player se mueve de arriba a abajo, entonces tenemos que poner la posicion inicial desde abajo para poder esquivar a los enemigos con facilidad
+tiempo_pasado_items = 0  # Tiempo transcurrido desde la aparición del último item
+tiempo_entre_items = 10000  # Tiempo entre la aparición de items en milisegundos
 
-enemigos = []  # Creamos esta variable, para tener una lista de todos los enemigos que aparezcan en la pantalla, para poder tackear su posicion, su velocidad...
-enemigos.append(Enemigo(ANCHO / 2, 100))  # Estas coordenadas nos llevaran a la posicion donde aparecera el enemigo
+cubo = Cubo(ANCHO / 2, ALTO - 80)  # Posición inicial del jugador
 
-balas = []  # Creamos esta variable para tener una lista de todas las balas que se disparen
+enemigos = []  # Lista para almacenar enemigos
+enemigos.append(Enemigo(ANCHO / 2, 100))  # Añadir un primer enemigo
+
+items = []  # Lista para almacenar items de vida
+
+balas = []  # Lista para almacenar balas disparadas por el jugador
 
 ultima_bala = 0  # Para controlar el tiempo entre disparos
 tiempo_entre_balas = 1  # Tiempo en milisegundos entre cada disparo
@@ -45,92 +50,108 @@ def crear_bala():
     global ultima_bala
 
     if pygame.time.get_ticks() - ultima_bala > tiempo_entre_balas:
-        balas.append(Bala(cubo.rect.centerx, cubo.rect.centery))  # Estas coordenadas nos llevaran a la posicion donde aparecera la bala (Desde el cubo)
+        balas.append(Bala(cubo.rect.centerx, cubo.rect.centery))  # Crear una nueva bala en la posición del jugador
         ultima_bala = pygame.time.get_ticks()
+        SONIDO_DISPARO.play()
 
 
-def gestionar_teclas(teclas):  # Lo que va a hacer es comprobar si dentro de nuestas teclas hay un
-    """
-    if teclas[pygame.K_w] :    # Con esto nos referimos a una tecla especifica
-        cubo.y -= cubo.velocidad    # Si pulsamos la "w" queremos que la posicion "y" de nuestro cubo se reste (vaya hacia arriba)
-    if teclas[pygame.K_s] :
-        cubo.y += cubo.velocidad    # Si pulsamos la "s" queremos que la posicion "y" de nuestro cubo se sume (vaya hacia abajo)
-    """
-    if teclas[pygame.K_a] and cubo.x > 0:  # Limita el movimiento hacia la izquierda
-        cubo.x -= cubo.velocidad  # Si pulsamos la "a" queremos que la posicion "x" de nuestro cubo se reste (vaya hacia la izquieda)
-    if teclas[pygame.K_d] and cubo.x < ANCHO - cubo.ancho:  # Limita el movimiento hacia la derecha
-        cubo.x += cubo.velocidad  # Si pulsamos la "d" queremos que la posicion "x" de nuestro cubo se sume (vaya hacia derecha)
+def gestionar_teclas(teclas):  # Comprobar el estado de las teclas
+    if teclas[pygame.K_a] and cubo.x > 0:  # Limitar el movimiento hacia la izquierda
+        cubo.x -= cubo.velocidad
+    if teclas[pygame.K_d] and cubo.x < ANCHO - cubo.ancho:  # Limitar el movimiento hacia la derecha
+        cubo.x += cubo.velocidad
 
 
+while jugando and vida > 0:  # Bucle principal del juego
 
-while jugando and vida > 0:  # La funcion principal del bucle es que jugaremos si nuestra vida es superior a 0, es decir, si nuestra vida es 0 no podremos jugar
+    tiempo_pasado_enemigos += reloj.tick(FPS)  # Incrementar el tiempo pasado en cada ciclo para controlar la aparición de enemigos
+    tiempo_pasado_items += reloj.get_time()  # Incrementar el tiempo pasado para controlar la aparición de items
 
-    tiempo_pasado += reloj.tick(FPS)  # Lo que estamos haciendo es que en tiempo_pasado estan añadiendose todos los milisegundos que pasan
+    # Crear un nuevo enemigo si ha pasado el tiempo suficiente
+    if tiempo_pasado_enemigos > tiempo_entre_enemigos:
+        enemigos.append(Enemigo(random.randint(0, ANCHO), -50))  # Crear un enemigo en una posición aleatoria en la parte superior
+        tiempo_pasado_enemigos = 0  # Reiniciar el contador de tiempo para enemigos
 
-    if tiempo_pasado > tiempo_entre_enemigos:  # Si tiempo_pasado es mayor al tiempo_entre_enemigos,
-        enemigos.append(Enemigo(random.randint(0, ANCHO), -50))  # Crearemos un nuevo enemigo en la parte de arriba de manera random
-        tiempo_pasado = 0  # Para no crear un bucle infinito, una vez el tiempo_pasado pasa a tiempo_entre_enemigo pues esta creando infinitos enemigos
+    # Crear un nuevo item si ha pasado el tiempo suficiente
+    if tiempo_pasado_items > tiempo_entre_items:
+        items.append(Item(random.randint(0, ANCHO), -50))  # Crear un item en una posición aleatoria en la parte superior
+        tiempo_pasado_items = 0  # Reiniciar el contador de tiempo para items
 
-    eventos = pygame.event.get()  # Esto nos va a devolver una lista con todos lo eventos que hay
+    eventos = pygame.event.get()  # Obtener todos los eventos del juego
 
-    teclas = pygame.key.get_pressed()  # Esto va a contener nuestras teclas pulsadas, esto lo que hace es formar una lista con las teclas que se pulsan
+    teclas = pygame.key.get_pressed()  # Obtener las teclas presionadas
 
     texto_vidas = FUENTE.render(f"Vida: {vida}", True, "White")
     texto_puntos = FUENTE.render(f"Puntos: {puntos}", True, "White")
 
-    cuidado = FUENTE.render(f"DANGEROUS", True, "Red")
-
-    gestionar_teclas(teclas)
+    gestionar_teclas(teclas)  # Mover el jugador de acuerdo a las teclas presionadas
 
     for evento in eventos:
-        if evento.type == pygame.QUIT:  # Esto puede suceder, si nos salimos de la pantalla, pulsamos ALT + F4, apagamos el ordenador
+        if evento.type == pygame.QUIT:  # Salir del juego si se cierra la ventana
             jugando = False
-        if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:  # Detectar disparo con la barra espaciadora
+        if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:  # Crear una bala si se presiona la barra espaciadora
             crear_bala()
-
 
     VENTANA.blit(fondo, (0, 0))  # Dibujar la imagen de fondo
 
-    cubo.dibujar(VENTANA)  # Cada vez que de una vuelta a nuestro bucle principal
+    cubo.dibujar(VENTANA)  # Dibujar al jugador en la pantalla
 
-    for enemigo in enemigos:  # Vamos a actualizar las posiciones de los enemigos
+    # Manejo de enemigos
+    for enemigo in enemigos:
         enemigo.dibujar(VENTANA)
-        enemigo.movimiento()  # Esto lo que hara sera recibir el movimiento del enemigo
+        enemigo.movimiento()
 
-        if pygame.Rect.colliderect(cubo.rect, enemigo.rect):  # Esto sirve para ver si nuestro player esta colisionando con un enemigo - .rect, quiere decir que va a hacer el rectangulo de cada personaje individualmente
-            print("Tocado")  # Si esto da verdadero, tenemos que imprimir "Tocado"
-            vida -= 1  # Ademas, de quitarnos 1 vida de la cantidad total que tenemos
-            VENTANA.blit(cuidado, (20, 120))  # Cada vez que nos quiten 1 vida, lo imprimiremos por pantalla para tener siempre constancia de las que nos quedan
-            enemigos.remove(enemigo)  # Y si chocamos contra un enemigo en lugar de sopbrepasarlo, este se eliminara automaticamente cuando nos quite 1 vida
+        # Colisión del jugador con un enemigo
+        if pygame.Rect.colliderect(cubo.rect, enemigo.rect):
+            print("Tocado")
+            vida -= 1  # Quitar una vida al jugador
+            enemigos.remove(enemigo)  # Eliminar el enemigo que colisionó
 
-        if enemigo.y > ALTO:     # Cuando un enemigo supere esta barrera, automaticamente desaparecera.
-            enemigos.remove(enemigo),           # el programa podria dejar de funcionar por excisiva cantidad de entidades
+        # Eliminar enemigo si sale de la pantalla
+        if enemigo.y > ALTO:
+            enemigos.remove(enemigo)
 
+        # Colisión de una bala con un enemigo
         for bala in balas:
-            if pygame.Rect.colliderect(bala.rect, enemigo.rect):  # Esto sirve para ver si nuestro player esta colisionando con un enemigo - .rect, quiere decir que va a hacer el rectangulo de cada personaje individualmente
-                balas.remove(bala)  # Y si chocamos contra un enemigo en lugar de sopbrepasarlo, este se eliminara automaticamente cuando nos quite 1 vida
-                enemigo.vida -= 1   # Esto hara que cuando una bala impacte contra el enmigo le bajara 1 de las 3 vidas que tiene
+            if pygame.Rect.colliderect(bala.rect, enemigo.rect):
+                balas.remove(bala)
+                enemigo.vida -= 1
 
-        if enemigo.vida <= 0:           # Al hacer el paso de arriba, de que cada vez que el enmigo es alcanzado por una bala, este le quitara 1 vida,
-            enemigos.remove(enemigo)    # tendriamos que hacer que cuando el enemigo llegue a 0 vidas, este sera eliminado.
-            puntos += 1
-            
+        if enemigo.vida <= 0:
+            enemigos.remove(enemigo)
+            puntos += 1  # Sumar un punto por cada enemigo eliminado
+
+    # Manejo de items
+    for item in items:
+        item.dibujar(VENTANA)
+        item.movimiento()
+
+        # Colisión del jugador con un item
+        if pygame.Rect.colliderect(cubo.rect, item.rect):
+            print("¡Vida extra!")
+            vida += 1  # Aumentar una vida al jugador
+            items.remove(item)  # Eliminar el item que fue recogido
+
+        # Eliminar item si sale de la pantalla
+        if item.y > ALTO:
+            items.remove(item)
+
+    # Manejo de balas
     for bala in balas:
         bala.movimiento()
         bala.dibujar(VENTANA)
-        # Elimina las balas que salen de la pantalla
-        if bala.y < 0:
+        if bala.y < 0:  # Eliminar las balas que salen de la pantalla
             balas.remove(bala)
 
     VENTANA.blit(texto_vidas, (20, 20))
     VENTANA.blit(texto_puntos, (20, 70))
 
-    pygame.display.update()  # Actualiza nuestra pantalla constantemente
+    pygame.display.update()  # Actualizar la pantalla
+
+SONIDO_MUERTE.play()
 
 pygame.quit()
-nombre = input("Introduce tu nombre: ")             # Nos pedira un nombre, para guardalos junto con nuestra puntuacion en un archivo txt
+nombre = input("Introduce tu nombre: ")
 
-with open("puntuaciones.txt", "a") as archivo:      # Abre el txt (puntuaciones.txt) *"a" -> .append* 
-    archivo.write(f"{nombre} - {puntos}\n")           # Esto sera el mensaje que se guarde en el archivo txt
-
-pygame.quit()  # Salir de Pygame
+with open("puntuaciones.txt", "a") as archivo:
+    archivo.write(f"{nombre} - {puntos}\n")
